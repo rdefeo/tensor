7179f8f4647acb6ae517390d503b7589a05370f7
Alessio Capitanelli <alessio.capitanelli@gmail.com>
2015-12-03 15:32:20 +0100

    improved visualization

--- improc/main.py
+++ improc/main.py
@@ -4,6 +4,10 @@ from os.path import isfile, join
 import itertools as it
 import numpy as np
 
+from cv2 import putText
+from cv2 import FONT_HERSHEY_SIMPLEX
+from cv2 import LINE_AA
+from cv2 import COLOR_GRAY2BGR
 from cv2 import waitKey
 from cv2 import imshow
 from cv2 import imread
@@ -43,7 +47,7 @@ def preprocess_basic(_image_files):
         img = preprocess.autocrop(img)
         img = preprocess.scale_max(img)
         img = preprocess.make_square(img)
-        img = preprocess.grey(img)
+        #img = preprocess.grey(img)
         processed_images.append(img)
 
     mosaic_image = mosaic(len(processed_images), processed_images)
@@ -81,7 +85,20 @@ def preprocess_zernike(_image_files):
     return mosaic_images
 
 
-def preprocess_negative(_image_files):
+def preprocess_canny(_image_files):
+    processed_images = []
+    for image_file in _image_files:
+        img = imread(image_file)
+        img = preprocess.autocrop(img)
+        img = preprocess.scale_max(img)
+        img = preprocess.make_square(img)
+        img = preprocess.grey(img)
+        img = preprocess.canny(img)
+        processed_images.append(img)
+    mosaic_images = mosaic((len(processed_images)), processed_images)
+    return mosaic_images
+
+def preprocess_neg_canny(_image_files):
     processed_images = []
     for image_file in _image_files:
         img = imread(image_file)
@@ -90,6 +107,7 @@ def preprocess_negative(_image_files):
         img = preprocess.make_square(img)
         img = preprocess.grey(img)
         img = preprocess.bitwise(img)
+        img = preprocess.canny(img)
         processed_images.append(img)
     mosaic_images = mosaic((len(processed_images)), processed_images)
     return mosaic_images
@@ -105,15 +123,25 @@ image_files = [join(shoes_path, f) for f in listdir(shoes_path) if isfile(join(s
 # waitKey(0)
 
 original_imgs = preprocess_basic(image_files)
-gray_imgs = preprocess_super_simple(image_files)
-reverse_imgs = preprocess_negative(image_files)
-zernike_images = preprocess_zernike(image_files)
+putText(original_imgs, 'Original', (10, 220), FONT_HERSHEY_SIMPLEX, 1, (250, 0, 200), 2, LINE_AA)
+
+canny_imgs = preprocess_canny(image_files)
+canny_imgs = cvtColor(canny_imgs, COLOR_GRAY2BGR)
+putText(canny_imgs, 'Canny images', (10, 220), FONT_HERSHEY_SIMPLEX, 1, (250, 0, 200), 2, LINE_AA)
+
+reverse_imgs = preprocess_neg_canny(image_files)
+reverse_imgs = cvtColor(reverse_imgs, COLOR_GRAY2BGR)
+putText(reverse_imgs, 'Bitwise_not + Canny', (10, 220), FONT_HERSHEY_SIMPLEX, 1, (250, 0, 200), 2, LINE_AA)
+
+zernike_imgs = preprocess_zernike(image_files)
+zernike_imgs = cvtColor(zernike_imgs, COLOR_GRAY2BGR)
+putText(zernike_imgs, 'Zernike', (10, 220), FONT_HERSHEY_SIMPLEX, 1, (250, 0, 200), 2, LINE_AA)
 
 # print original_imgs.shape
-# print gray_imgs.shape
-# print zernike_images.shape
+# print reverse_imgs.shape
+# print zernike_imgs.shape
 
-comparison = np.concatenate((original_imgs, gray_imgs, reverse_imgs, zernike_images), axis=0)
+comparison = np.concatenate((original_imgs, canny_imgs, reverse_imgs, zernike_imgs), axis=0)
 imshow("Descriptors comparison", comparison)
 waitKey(0)
 
