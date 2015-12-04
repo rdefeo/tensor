4b97b0cdd476aa571c892a787f74d5791dbbd98f
Alessio Capitanelli <alessio.capitanelli@gmail.com>
2015-12-04 16:46:00 +0100

    added db update

--- grabber/grab.py
+++ grabber/grab.py
@@ -25,33 +25,59 @@ if not(path.exists(out_dir)):
 products = collection.find()
 
 print "Downloading images..."
-prd = 0; print_interval = products.count()/20
+print_interval = products.count()/20
 
-for product in products:
-    product_name = product['_id']
+for prd_index, product in enumerate(products):
+    product_status = "ok"
+    product_id = product['_id']
 
-    if prd % print_interval == 0:
-        print(str(prd / print_interval * 5) + "% of products scanned")
+    if prd_index % print_interval == 0:
+        print(str(prd_index / print_interval * 5) + "% of products scanned")
 
-    product_img_num = -1
-    for img in product['images']:
+    set_data = {}
+    for img_index, img in enumerate(product['images']):
+        img_status = None
         url = img['url']
-        product_img_num += 1
+        img_id = img['_id']
+        img_filename = out_dir + "/" + str(product_id) + "_" + str(img_id) + ".jpg"
 
-        # image fetch
-        if not(path.isfile(out_dir + "/" + str(product_name) + "_" + str(product_img_num) + ".jpg")):  # check img exist
+        if "image_processed_status" not in img:  # check previously processed img
+
+            # image fetch
             img_data = requests.get(url, stream=True)
+
             if img_data.status_code == 200:
-
                 # image processing
-                image = imdecode(np.fromstring(img_data.content, np.uint8), flags=IMREAD_COLOR)
-                image = preprocess.autocrop(image)
-                image = preprocess.scale_max(image)
-                image = preprocess.make_square(image)
-                imshow("img", image)
-                waitKey(0)
+                try:
+                    image_decoded = imdecode(np.fromstring(img_data.content, np.uint8), flags=IMREAD_COLOR)
+                    image_autocropped = preprocess.autocrop(image_decoded)
+                    image_squared = None
+                    if image_autocropped is not None:
+                        image_scaled_max = preprocess.scale_max(image_autocropped)
+                        image_squared = preprocess.make_square(image_scaled_max)
+                        imshow("img", image_squared)
+                        imwrite(img_filename, image_squared)  # save image
+                        waitKey(0)
+                        img_status = "ok"
+                    else:
+                        img_status = "autocropped_failed"
+                        product_status = "failed"
 
-                image_name = out_dir + "/" + str(product_name) + "_" + str(product_img_num) + ".jpg"  # save image
-                imwrite(image_name, image)
+                except AttributeError:
+                    print("Unable to process image " + str(img_index) + "/" + str(prd_index))
+                    continue
 
-    prd += 1
+            else:
+                img_status = "http_fail"
+                product_status = "failed"
+                print("Unable to retrieve image " + str(img_index) + "/" + str(prd_index))
+                continue
+
+            set_data["images.%s.image_processed_status" % img_index] = img_status  # update img status
+
+        if any(set_data):
+            collection.update(
+                {"_id": product_id},
+                {"$set": set_data},
+                upsert=False
+            )
