dded2d0abdbafb7b2e2fa2b265bb1fc2b42b7024
Alessio Capitanelli <alessio.capitanelli@gmail.com>
2015-12-05 21:53:18 +0100

    added statistics

--- grabber/grab.py
+++ grabber/grab.py
@@ -58,13 +58,18 @@ products = collection.find()
 
 print "Downloading images..."
 print_interval = products.count()/20
+num_processed_products = 0
+num_processed_imgs = 0
+num_corrupted_imgs = 0
+num_failed_crop = 0
+
 
 for prd_index, product in enumerate(products):
     product_status = "ok"
     product_id = product['_id']
 
     if prd_index % print_interval == 0:
-        print(str(prd_index / print_interval * 5) + "% of products scanned")
+        print(str(prd_index / print_interval * 4) + "% of products scanned")
 
     set_data = {}
     for img_index, img in enumerate(product['images']):
@@ -75,7 +80,7 @@ for prd_index, product in enumerate(products):
 
         if "image_processed_status" not in img:  # check if image already exist
 
-            img_data = requests.get(url)
+            img_data = requests.get(url, stream=True)
 
             if img_data.status_code == 200:
                 processed_img = None
@@ -85,6 +90,7 @@ for prd_index, product in enumerate(products):
                 else:
                     img_status = "image_corrupted"
                     product_status = "failed"
+                    num_corrupted_imgs += 1
 
                 if processed_img is not None:
                     imwrite(img_filename, processed_img)  # save image
@@ -92,6 +98,7 @@ for prd_index, product in enumerate(products):
                 else:
                     img_status = "autocropped_failed"
                     product_status = "failed"
+                    num_failed_crop += 1
 
             else:
                 img_status = "http_fail"
@@ -101,6 +108,14 @@ for prd_index, product in enumerate(products):
             set_data["images.%s.image_processed_status" % img_index] = img_status  # update img status
 
         update_product(set_data, product_id)
+        num_processed_imgs += 1
 
     set_data["processed_status"] = product_status  # update_product_status
     update_product(set_data, product_id)
+    num_processed_products += 1
+
+print "100% of products scanned\n"
+print "Total number of processed products: %i" % num_processed_products
+print "Total number of processed images: %i" % num_processed_imgs
+print "Number of images failed to crop: %i" % num_failed_crop
+print "Number of corrupted images: %i" % num_corrupted_imgs
